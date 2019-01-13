06730417089db0477b2f85cbc16d69409990e27b
Kaloli <rvellark@gmail.com>
2019-01-13 13:56:24 -0700

    update dec 13 1356

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var config = {
     apiKey: "AIzaSyBGUDlQUiPPCqaHZu6_Qr_zNOa09P84O3M",
     authDomain: "trainapp-83721.firebaseapp.com",
     databaseURL: "https://trainapp-83721.firebaseio.com",
-    projectId: "trainapp-83721",
+    
     storageBucket: "trainapp-83721.appspot.com",
     messagingSenderId: "68137495036"
   };
@@ -32,4 +32,23 @@ var config = {
         $("#frequencyInput").val("");
         
         return false;
+    })
+
+    trainData.ref().on("child_added",function(snapshot){
+        var name = snapshot.val().name;
+        var destination = snapshot.val().destination;
+        var frequency = snapshot.val().frequency;
+        var firstTrain = snapshot.val().firstTrain;
+        
+
+        var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
+        var minutes = frequency - remainder;
+        var arrival = moment().add(minutes,"m").format("hh:mm A");
+
+      console.log(remainder);
+      console.log(minutes);
+      console.log(arrival);
+
+      $("#trainTable > tBody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
+        
     })
